0bdd0effcab0963661cd64d61d8cc5e4da6906b3
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-11-05 18:10:27 +0330

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import eslint from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  perfectionist.configs["recommended-natural"]
+);
