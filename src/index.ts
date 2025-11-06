79a81a1abb04605e0a916e9a8c30feabce38f90f
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-11-06 12:41:09 +0330

    Deploying to vercel

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
 import artistsRoute from "@routes/artists.route"
 import favoritesRoute from "@routes/favorites.route"
 import genresRoute from "@routes/genres.route"
-import playlistRoute from "@routes/playlist.route"
+import playlistsRoute from "@routes/playlists.route"
 import songsRoute from "@routes/songs.route"
 import usersRoute from "@routes/users.route"
 import express from "express"
 import middlewares from "middlewares"
+import serverless from "serverless-http"
 import "dotenv/config"
 
 const app = express()
 
 app.use(middlewares)
-
 app.use("/api/users", usersRoute)
 app.use("/api/songs", songsRoute)
 app.use("/api/genres", genresRoute)
 app.use("/api/artists", artistsRoute)
-app.use("/api/playlist", playlistRoute)
+app.use("/api/playlist", playlistsRoute)
 app.use("/api/favorites", favoritesRoute)
-
 app.use((_, res) => res.sendStatus(404))
 
-const PORT = process.env["PORT"]
-const SERVER_URL = process.env["SERVER_URL"]
-
-app.listen(PORT, () => console.log(`Server running on ${SERVER_URL}`))
+export const handler = serverless(app)
+export default app
---
+++ src/routes/playlists.route.ts
@@ -0,0 +1,8 @@
+// import { resolveAuthToken } from "@middlewares/custom.mw"
+import { Router } from "express"
+
+const route = Router()
+
+// route.post("/", resolveAuthToken, async (req, res) => {})
+
+export default route
